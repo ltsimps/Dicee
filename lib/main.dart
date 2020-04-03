e25ba846980f8252401cf156079c7280b6bd8739
ltsimps <ltsimpso@me.com>
2020-04-03 16:04:25 -0400

    add random functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,15 +10,68 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DicePage(
+
+
+        ),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  static final randomDiceNumber  = new Random();
+  int leftDiceNumber  = randomDiceNumber.nextInt(6) + 1;
+  int rightDiceNumber = randomDiceNumber.nextInt(6) + 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+
+            child: FlatButton(
+              onPressed: () {
+
+
+                setState(() {
+                  leftDiceNumber = randomDiceNumber.nextInt(6) + 1;
+                });
+
+                print('left dice pressed');
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),),
+          ),
+          Expanded(
+
+            child: FlatButton(
+                onPressed: ()  {
+
+                  setState(() {
+                    rightDiceNumber = randomDiceNumber.nextInt(6) + 1;
+                  });
+
+                  print("right dice pressed");
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+
+        ],
+      ),
+    );
   }
+
+
 }
+
